74728b8a0151579a10b78180f48018f69d3120ad
Hooseon Yim <hoo.yim@gmail.com>
2018-02-03 20:59:27 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  list: any;
+  listAvailable = false;
+  food: string;
+  restaurant = {
+  
+    "foodWords": ["pepperoni pizza","sushi","kimchi"],
+    "location": {
+      "lat":"45.5036174", 
+      "lng":"-73.5798482"
+    },
+    "radius": 1000
+  
+  }
+
+  constructor(private http: Http) {
+    
+  }
+
+  getData() {
+    this.http.get('http://sate.us-west-2.elasticbeanstalk.com/api/food/insertWord/').map(res => res.json()).subscribe((data) => {
+      console.log(data);
+      this.list = data;
+      this.listAvailable = true;
+    }, err => {
+      console.log(err);
+      this.listAvailable = false;
+    })
+  }
+
+  searchFood() {
+    this.http.post('http://sate.us-west-2.elasticbeanstalk.com/api/food/insert/', this.restaurant).map(res => res.json()).subscribe(data => {
+      this.list = data;
+      this.listAvailable = true;
+      console.log(data);
+    }, err => {
+      this.listAvailable = false;
+      console.log(err);
+    });
+  }
+
+
+
+
 }
